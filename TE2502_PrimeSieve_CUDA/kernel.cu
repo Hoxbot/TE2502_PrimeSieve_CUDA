58567d5a0194879d33aec0a617c8bdea390cf46a
Hoxbot <hoxbot@unseen.is>
2020-10-06 16:09:50 +0200

    Add project files.

---
+++ TE2502_PrimeSieve_CUDA/kernel.cu
@@ -0,0 +1,40 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+#include <iostream>
+#include <sstream>
+
+//For memory leaks
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+//Misc
+inline void WaitForEnter() {
+	std::string str;
+	std::cout << "Enter to continue..." << std::endl;
+	std::getline(std::cin, str);
+}
+
+int main() {
+	//Check for memory leaks at each exit point of the program
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//---
+
+
+
+	//---
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+	cudaError_t cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+	WaitForEnter();
+
+    return 0;
+}
---
+++ TE2502_PrimeSieve_CUDA/sieves/sieve_base.h
@@ -0,0 +1,10 @@
+#include <vector>
+
+class SieveBase {
+private:
+
+
+public:
+
+
+};
